5586f52588b3d4c13df2f39cd1603da7f66c3249
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-25 03:24:29 +0300

    Update App

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Navigation from './app/navigation';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <Navigation />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
