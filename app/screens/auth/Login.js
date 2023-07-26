bbfdd6723f29ea863d029a2acf46cc8647c41428
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-27 02:50:00 +0300

    Update Login.js

--- app/screens/auth/Login.js
+++ app/screens/auth/Login.js
@@ -1,14 +1,155 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Formik } from 'formik';
+import { loginValidationSchema } from '../../components/Auth/Validation';
 
-const Login = () => {
+const Login = ({ navigation }) => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={require('../../../assets/images/login.png')} style={styles.image} />
+        <Text style={styles.title}>English Tales</Text>
+      </View>
+      <View style={styles.formContainer}>
+        <Text style={styles.subtitle}>Email</Text>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values) => console.log(values)}
+          validationSchema={loginValidationSchema}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your email"
+                placeholderTextColor="#fff"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+              />
+              {errors.email && <Text style={styles.errors}>{errors.email}</Text>}
+              <Text style={styles.subtitle}>Password</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your password"
+                placeholderTextColor="#fff"
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              />
+              {errors.password && <Text style={styles.errors}>{errors.password}</Text>}
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Log In</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Formik>
+        <View>
+          <Text style={styles.infoText}>Forgot password?</Text>
+        </View>
+      </View>
+      <View style={styles.signupContainer}>
+        <Text style={styles.signupInfo}>Don't have an account?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+          <Text style={styles.signupText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+    padding: 5,
+  },
+  imageContainer: {
+    alignItems: 'center',
+  },
+  image: {
+    width: 290,
+    height: 290,
+    borderRadius: 6,
+    resizeMode: 'cover',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '600',
+    color: '#fff',
+    lineHeight: 52,
+    textAlign: 'center',
+  },
+  formContainer: {
+    marginHorizontal: 20,
+    padding: 20,
+  },
+  subtitle: {
+    color: '#fff',
+    fontSize: 14,
+    lineHeight: 18,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  input: {
+    width: 311,
+    height: 48,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#333333',
+    borderRadius: 6,
+    backgroundColor: '#161616',
+    color: '#bdbdbd',
+    fontSize: 14,
+    lineHeight: 19,
+  },
+  button: {
+    width: 311,
+    height: 48,
+    padding: 8,
+    marginTop: 20,
+    backgroundColor: '#000000',
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 14,
+    lineHeight: 16,
+    fontWeight: 'bold',
+  },
+  infoText: {
+    color: '#fff',
+    fontSize: 12,
+    lineHeight: 16,
+    textAlign: 'right',
+    fontWeight: '300',
+    marginTop: 10,
+  },
+  signupContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  signupInfo: {
+    color: '#fff',
+    fontSize: 14,
+    lineHeight: 18,
+    marginRight: 5,
+  },
+  signupText: {
+    color: '#FFA500',
+    fontSize: 14,
+    lineHeight: 18,
+    fontWeight: 'bold',
+  },
+  errors: {
+    fontSize: 14,
+    color: 'red',
+    marginTop: 3,
+    fontWeight: 'normal',
+  },
+});
