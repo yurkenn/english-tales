3df56fefe61b5ee539e7a7382c78aefd0379691f
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-29 10:12:57 +0300

    Update index.js

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -1,18 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native';
 
-import React from 'react';
+import React, { useContext } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthNavigation from './AuthNavigation';
 import TabNavigation from './TabNavigation';
+import { AuthContext } from '../store/auth-context';
+import { Colors } from '../constants/colors';
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
+  const authContext = useContext(AuthContext);
   return (
     <NavigationContainer
       theme={{
         colors: {
-          background: '#161616',
+          background: Colors.primaryBackground,
         },
       }}
     >
@@ -21,8 +24,11 @@ const Navigation = () => {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="Auth" component={AuthNavigation} />
-        <Stack.Screen name="Tab" component={TabNavigation} />
+        {authContext.user ? (
+          <Stack.Screen name="Tab" component={TabNavigation} />
+        ) : (
+          <Stack.Screen name="Auth" component={AuthNavigation} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
