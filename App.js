e5668bdfb88c405f0d8cfd63d27bb9785cfaea22
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-29 10:12:43 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Navigation from './app/navigation';
 import { StatusBar } from 'expo-status-bar';
+import { AuthProvider } from './app/store/auth-context';
 
 const App = () => {
   return (
     <>
-      <Navigation />
       <StatusBar style="auto" />
+      <AuthProvider>
+        <Navigation />
+      </AuthProvider>
     </>
   );
 };
