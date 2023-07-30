282f88aebdefeece854a9d15c1d75090ad340015
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-30 23:31:30 +0300

    Update SignupAnimation.js

--- app/components/Animations/SignupAnimation.js
+++ app/components/Animations/SignupAnimation.js
@@ -2,5 +2,17 @@ import React from 'react';
 import LottieView from 'lottie-react-native';
 
 export default function SignupAnimation() {
-  return <LottieView source={require('../../../assets/animations/signup.json')} autoPlay loop />;
+  return (
+    <LottieView
+      style={{
+        width: 290,
+        height: 290,
+        borderRadius: 6,
+        resizeMode: 'cover',
+      }}
+      source={require('../../../assets/animations/signup.json')}
+      autoPlay
+      loop
+    />
+  );
 }
