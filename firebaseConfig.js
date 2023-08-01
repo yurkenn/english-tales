a6d5ecb72009a27e50c59b4a0f9478234845b8ca
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-08-01 03:20:29 +0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// Optionally import the services that you want to use
+import { getAuth } from 'firebase/auth';
+// import {...} from "firebase/database";
+import { getFirestore } from 'firebase/firestore';
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+import Config from 'react-native-config';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: Config.API_KEY,
+  authDomain: Config.AUTH_DOMAIN,
+  projectId: Config.PROJECT_ID,
+  storageBucket: Config.STORAGE_BUCKET,
+  messagingSenderId: Config.MESSAGING_SENDER_ID,
+  appId: Config.APP_ID,
+  measurementId: Config.MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const firestore = getFirestore();
