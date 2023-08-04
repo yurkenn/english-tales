d40babececf5e96e6fc48a223623eb1509bcb2fc
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-08-04 08:14:07 +0300

    Update index.js

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -1,41 +1,47 @@
 import { NavigationContainer } from '@react-navigation/native';
-
 import React, { useContext } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthNavigation from './AuthNavigation';
 import TabNavigation from './TabNavigation';
+import Content from '../screens/Content';
 import { AuthContext } from '../store/auth-context';
 import { Colors } from '../constants/colors';
-import { ActivityIndicator, View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Tab"
+        component={TabNavigation}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Content"
+        component={Content}
+        options={({ route }) => ({
+          //title: route.params.data.title,
+          headerTitle: '',
+          headerTransparent: true,
+          headerBackTitleVisible: false,
+          headerTintColor: '#fff',
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const Navigation = () => {
   const authContext = useContext(AuthContext);
 
-  if (authContext.loading) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <ActivityIndicator size={'large'} />
-      </View>
-    );
-  }
-
   return (
-    <NavigationContainer
-      theme={{
-        colors: {
-          background: Colors.primaryBackground,
-        },
-      }}
-    >
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
+    <NavigationContainer theme={{ colors: { background: Colors.primaryBackground } }}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         {authContext.userInfo ? (
-          <Stack.Screen name="Tab" component={TabNavigation} />
+          <Stack.Screen name="Home" component={HomeStack} />
         ) : (
           <Stack.Screen name="Auth" component={AuthNavigation} />
         )}
