fcb08c342405cb99e0d125ebac5bb82307b0c83c
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-08-04 08:14:00 +0300

    Update FeaturedStories.js

--- app/components/Home/FeaturedStories.js
+++ app/components/Home/FeaturedStories.js
@@ -1,16 +1,23 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { Colors } from '../../constants/colors';
+import { BookmarkOutlineIcon, LikeIcon } from '../../UI/Icons';
 import { BookmarkIcon } from '../../UI/SvgIcons';
+import { useNavigation } from '@react-navigation/native';
+import BookmarkButton from '../../UI/BookmarkButton';
 
 const FeaturedStories = ({ data }) => {
+  const navigation = useNavigation();
   const { title, author, bookmarks, category, content, imageUrl, likes, timestamp } = data;
+
+  const handleReadButton = () => {
+    navigation.navigate('Content', { data });
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <TouchableOpacity style={styles.button}>
-          <BookmarkIcon />
-        </TouchableOpacity>
+        <BookmarkButton />
         <Image source={{ uri: imageUrl }} style={styles.image} />
         <View style={styles.authorContainer}>
           <Text style={styles.author}>{author}</Text>
@@ -21,8 +28,19 @@ const FeaturedStories = ({ data }) => {
         <Text style={styles.category}>{category}</Text>
       </View>
       <View style={styles.infoContainer}>
-        <Text style={styles.likes}>{likes}</Text>
-        <Text style={styles.bookmarks}>{bookmarks}</Text>
+        <View style={styles.likeContainer}>
+          <LikeIcon />
+          <Text style={styles.likes}>{likes}</Text>
+        </View>
+        <View style={styles.bookmarkContainer}>
+          <BookmarkOutlineIcon />
+          <Text style={styles.bookmarks}>{bookmarks}</Text>
+        </View>
+        <View style={styles.readButtonContainer}>
+          <TouchableOpacity onPress={handleReadButton} style={styles.readButton}>
+            <Text style={styles.readButtonText}>Read</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -32,31 +50,16 @@ export default FeaturedStories;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: Colors.cardBackground,
     borderRadius: 10,
     marginHorizontal: 10,
     padding: 10,
-    width: 300,
-    height: 270,
   },
 
   imageContainer: {
     position: 'relative',
   },
-  button: {
-    backgroundColor: '#000000',
-    flex: 1,
-    borderRadius: 6,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 32,
-    width: 32,
-    position: 'absolute',
-    right: 10,
-    top: 10,
-    zIndex: 1,
-  },
-
   image: {
     borderRadius: 10,
     height: 150,
@@ -91,16 +94,45 @@ const styles = StyleSheet.create({
   },
   infoContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  likeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 10,
   },
   likes: {
     color: Colors.white,
     fontSize: 12,
     fontWeight: 'bold',
+    marginLeft: 5,
+  },
+  readButtonContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  readButton: {
+    backgroundColor: Colors.primaryBackground,
+    borderRadius: 6,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 60,
+  },
+  readButtonText: {
+    color: Colors.white,
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  bookmarkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 10,
   },
   bookmarks: {
     color: Colors.white,
     fontSize: 12,
     fontWeight: 'bold',
+    marginLeft: 5,
   },
 });
