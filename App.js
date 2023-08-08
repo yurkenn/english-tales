a5ac1ad4f36768165a298949f26abb0b12a6a5cc
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-08-08 04:59:02 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from './app/navigation';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './app/store/auth-context';
+import 'react-native-url-polyfill/auto';
 
 const App = () => {
   return (
