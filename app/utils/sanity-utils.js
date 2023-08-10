769bc9ee9605f6e71e18679cf175110259e6b9dd
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-08-10 06:57:12 +0300

    Create sanity-utils.js

---
+++ app/utils/sanity-utils.js
@@ -0,0 +1,71 @@
+import { client } from '../../sanity';
+
+export const getFeatured = async () => {
+  try {
+    const featured = await client.fetch(`*[_type == "featured"]{
+      title,
+      description,
+       "imageURL" : imageURL.asset->url,
+      "tales": tales[]->{
+        title,
+        slug,
+        "category": categories[0]->title,
+        "author": author->name,
+        "imageURL": imageURL.asset->url,
+        "likes": likes,
+        "publishedAt": publishedAt
+      }
+    }`);
+    return featured;
+  } catch (error) {
+    console.error('Error fetching featured data:', error);
+    throw error;
+  }
+};
+
+export const getTales = async () => {
+  try {
+    const tales = await client.fetch(`*[_type == "tale"]{
+      title,
+      slug,
+      "imageURL": imageURL.asset->url
+    }`);
+    return tales;
+  } catch (error) {
+    console.error('Error fetching tales:', error);
+    throw error;
+  }
+};
+
+export const getTaleBySlug = async (slug) => {
+  try {
+    const tale = await client.fetch(
+      `*[_type == "tale" && slug.current == $slug]{
+        title,
+        slug,
+        "imageURL" : imageURL.asset->url, 
+        "author": author->name,
+        "authorImage": author->image,
+        "category": categories[0]->title,
+        "body": body
+      }`,
+      { slug }
+    );
+    return tale;
+  } catch (error) {
+    console.error('Error fetching tale by slug:', error);
+    throw error;
+  }
+};
+
+export const getCategories = async () => {
+  try {
+    const categories = await client.fetch(`*[_type == "category"]{
+      title
+    }`);
+    return categories;
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    throw error;
+  }
+};
