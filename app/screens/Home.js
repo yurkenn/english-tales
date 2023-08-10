90a633b1330569be4901fae95dc5840ab6cdd47b
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-08-10 06:57:10 +0300

    Update Home.js

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -10,76 +10,15 @@ import {
 import React, { useEffect, useState } from 'react';
 import FeaturedStories from '../components/Home/FeaturedStories';
 import Categories from '../components/Home/Categories';
-import useFetchTales from '../hooks/useFetchTales';
 import LoadingAnimation from '../components/Animations/LoadingAnimation';
 import ErrorAnimation from '../components/Animations/ErrorAnimation';
 import MyStories from '../components/Home/MyStories';
+import useGetFeaturedStories from '../hooks/useGetFeaturedStories';
+import useGetCategories from '../hooks/useGetCategories';
 
 const Home = () => {
-  const { tales, categories, loading, error } = useFetchTales();
-
-  const [data, setData] = useState([
-    {
-      id: '1',
-      title: 'The Adventure of the Speckled Band',
-      author: 'Arthur Conan Doyle',
-      bookmarks: '1.2k',
-      category: 'Adventure',
-      content:
-        'The Adventure of the Speckled Band is one of the 56 short Sherlock Holmes stories written by Scottish author Sir Arthur Conan Doyle. It is the eighth of the twelve stories collected in The Adventures of Sherlock Holmes. It is one of four Sherlock Holmes stories that can be classified as a locked room mystery. The story was first published in Strand Magazine in February 1892, with illustrations by Sidney Paget. It was published under the different title The Spotted Band in New York World in August 1905. Doyle later revealed that he thought this was his best Holmes story. Doyle wrote and produced a play based on the story. It premiered at the Adelphi Theatre, London on 4 June 1910, with H. A. Saintsbury as Sherlock Holmes and Lyn Harding as Dr. Grimesby Roylott. The play was revived twice in London, in 1918 and 1923. It was also produced twice on Broadway, in 1910 and 1958. The 1910 production, which opened at the Garrick Theatre on 5 September 1910, ran for 239 performances. The 1958 production, which opened at the Morosco Theatre on 12 October 1958, ran for 71 performances. The play was adapted for BBC Radio 4 in 1994, starring Clive Merrison as Holmes and Michael Williams as Watson. The story was adapted for The Adventures of Sherlock Holmes, an American radio show that aired from 1930 to 1936. The story was adapted for the 1984–1995 Sherlock Holmes series starring Jeremy Brett. The story was adapted for the 1984–1995 Sherlock Holmes series starring Jeremy Brett. The story was adapted for the 1984–1995 Sherlock Holmes series starring Jeremy Brett. The story was adapted for the 1984–1995 Sherlock Holmes series starring Jeremy Brett. The story was adapted for the 1984–1995 Sherlock Holmes series starring Jeremy Brett. The story was adapted for the 1984–1995 Sherlock Holmes series starring Jeremy Brett. The story was adapted for the 1984–1995 Sherlock Holmes series starring Jeremy Brett. The story was adapted for the 1984–1995 Sherlock Holmes series starring Jeremy Brett.1958, ran for 71 performances. The play was adapted for BBC Radio 4 in 1994, starring Clive Merrison as Holmes and Michael Williams as Watson. The story was adapted for The Adventures of Sherlock Holmes, an American radio show that aired from 1930 to 1936.',
-      imageUrl: 'https://source.unsplash.com/featured/?adventure',
-      likes: '2.3k',
-      lastRead: '01-01-2022',
-    },
-    {
-      id: '2',
-      title: 'The Adventure of the Speckled Band',
-      author: 'Arthur Conan Doyle',
-      bookmarks: '1.2k',
-      category: 'Adventure',
-      content:
-        'The Adventure of the Speckled Band is one of the 56 short Sherlock Holmes stories written by Scottish author Sir Arthur Conan Doyle. It is the eighth of the twelve stories collected in The Adventures of Sherlock Holmes. It is one of four Sherlock Holmes stories that can be classified as a locked room mystery.',
-      imageUrl:
-        'https://images.unsplash.com/photo-1504851149312-7a075b496cc7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8YWR2ZW50dXJlfHx8fHx8MTY5MTAyNjU2OA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
-      likes: '2.3k',
-      lastRead: '03-08-2023',
-    },
-    {
-      id: '3',
-      title: 'The Adventure of the Speckled Band',
-      author: 'Arthur Conan Doyle',
-      bookmarks: '1.2k',
-      category: 'Adventure',
-      content:
-        'The Adventure of the Speckled Band is one of the 56 short Sherlock Holmes stories written by Scottish author Sir Arthur Conan Doyle. It is the eighth of the twelve stories collected in The Adventures of Sherlock Holmes. It is one of four Sherlock Holmes stories that can be classified as a locked room mystery.',
-      imageUrl:
-        'https://images.unsplash.com/photo-1542235222-30e843cb43a1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8YWR2ZW50dXJlfHx8fHx8MTY5MTAyNjU3Mw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
-      likes: '2.3k',
-      lastRead: '01-08-2023',
-    },
-  ]);
-  const [data2, setdata2] = useState([
-    {
-      id: '1',
-      name: 'Adventure',
-    },
-    {
-      id: '2',
-      name: 'Horrors',
-    },
-    {
-      id: '3',
-      name: 'Romance',
-    },
-    {
-      id: '4',
-      name: 'Thriller',
-    },
-    {
-      id: '5',
-      name: 'Action',
-    },
-  ]);
+  const { featuredStories, loading, error } = useGetFeaturedStories();
+  const { categories } = useGetCategories();
 
   if (loading) {
     return <LoadingAnimation />;
@@ -94,17 +33,16 @@ const Home = () => {
       <View style={styles.featureContainer}>
         <Text style={styles.featureText}>Featured Stories</Text>
         <FlatList
-          data={data}
+          data={featuredStories}
           horizontal
           showsHorizontalScrollIndicator={false}
           renderItem={({ item }) => <FeaturedStories data={item} />}
-          keyExtractor={(item) => item.id}
         />
       </View>
       <View style={styles.categoriesContainer}>
         <Text style={styles.categoriesText}>Categories</Text>
         <FlatList
-          data={data2}
+          data={categories}
           horizontal
           showsHorizontalScrollIndicator={false}
           renderItem={({ item }) => <Categories data={item} />}
@@ -112,7 +50,7 @@ const Home = () => {
       </View>
       <View style={styles.myStoriesContainer}>
         <Text style={styles.myStoriesText}>My Stories</Text>
-        <MyStories data={data2} />
+        <MyStories data={categories} />
       </View>
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Explore All</Text>
