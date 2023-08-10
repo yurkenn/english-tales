3ed94b42fb7942f856f448e27418708e0d3365a6
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-08-10 08:15:24 +0300

    Update TaleContent.js

--- app/components/TaleContent.js
+++ app/components/TaleContent.js
@@ -3,10 +3,17 @@ import React from 'react';
 import PortableText from 'react-portable-text';
 
 const TaleContent = ({ blocks }) => {
-  console.log('BLOCKS: ', blocks);
   return (
     <View>
-      <PortableText content={blocks} />
+      <PortableText
+        content={blocks}
+        serializers={{
+          h1: ({ children }) => <Text style={styles.h1}>{children}</Text>,
+          li: ({ children }) => <Text style={styles.li}>{children}</Text>,
+          p: ({ children }) => <Text style={styles.p}>{children}</Text>,
+          normal: ({ children }) => <Text style={styles.normal}>{children}</Text>,
+        }}
+      />
     </View>
   );
 };
@@ -14,20 +21,8 @@ const TaleContent = ({ blocks }) => {
 export default TaleContent;
 
 const styles = StyleSheet.create({
-  h1: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  li: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  p: {
-    fontSize: 18,
-    marginBottom: 10,
-  },
-  normal: {
-    fontSize: 16,
-  },
+  h1: {},
+  li: {},
+  p: {},
+  normal: {},
 });
