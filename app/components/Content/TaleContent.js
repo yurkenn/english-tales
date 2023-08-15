e725c1ba02e52712c9760173dbbfa09b7532a6a5
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-08-15 06:13:54 +0300

    Create TaleContent.js

---
+++ app/components/Content/TaleContent.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import PortableText from 'react-portable-text';
+import { Colors } from '../../constants/colors';
+
+const TaleContent = ({ blocks }) => {
+  return (
+    <View>
+      <PortableText
+        content={blocks}
+        serializers={{
+          h1: ({ children }) => <Text style={styles.h1}>{children}</Text>,
+          li: ({ children }) => <Text style={styles.li}>{children}</Text>,
+          p: ({ children }) => <Text style={styles.p}>{children}</Text>,
+          normal: ({ children }) => <Text style={styles.normal}>{children}</Text>,
+        }}
+      />
+    </View>
+  );
+};
+
+export default TaleContent;
+
+const styles = StyleSheet.create({
+  h1: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.white,
+  },
+  li: {
+    fontSize: 16,
+    color: Colors.white,
+  },
+  p: {
+    fontSize: 16,
+    color: Colors.white,
+  },
+  normal: {
+    fontSize: 16,
+    color: Colors.white,
+    paddingBottom: 10,
+    letterSpacing: 0.5,
+  },
+});
