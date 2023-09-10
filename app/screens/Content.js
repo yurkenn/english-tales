935094ad63863a6751f045aecf0bb3f9e5a37972
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-09-10 13:53:35 +0300

    Update Content.js

--- app/screens/Content.js
+++ app/screens/Content.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { urlFor } from '../../sanity';
 import TaleContent from '../components/Content/TaleContent';
-import { AnimatedScrollView } from '@kanelloc/react-native-animated-header-scroll-view';
 import HeaderNavbar from '../components/Content/HeaderNavbar';
 import TopNavbar from '../components/Content/TopNavbar';
 import { Colors } from '../constants/colors';
 import useGetTale from '../hooks/useGetTale';
 import LoadingAnimation from '../components/Animations/LoadingAnimation';
 import ErrorAnimation from '../components/Animations/ErrorAnimation';
+import Icon from '../UI/Icons';
+import ParallaxScrollView from 'react-native-parallax-scroll-view';
+import { setLikes } from '../utils/sanity-utils';
 
 const Content = ({ route }) => {
   const { slug } = route.params;
@@ -20,13 +22,20 @@ const Content = ({ route }) => {
   // State for tracking like and bookmark status
   const [isLiked, setIsLiked] = useState(false);
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [likeCount, setLikeCount] = useState(tale[0]?.likes);
 
   // Function to handle liking
-  const handleLike = () => {
+  const handleLike = async () => {
     // Toggle like status
     setIsLiked(!isLiked);
 
-    // Implement logic to send like to your server or store it locally
+    // Implement logic to save the like to the database
+    try {
+      const updatedLikes = await setLikes(tale[0].slug.current, likeCount + 1);
+      setLikeCount(updatedLikes);
+    } catch (error) {
+      console.error('Error updating likes:', error);
+    }
   };
 
   // Function to handle bookmarking
@@ -43,48 +52,83 @@ const Content = ({ route }) => {
   return (
     <View style={styles.container}>
       {tale && (
-        <AnimatedScrollView
-          headerImage={{ uri: urlFor(tale[0].imageURL).url() }}
-          HeaderNavbarComponent={<HeaderNavbar title={tale[0].title} />}
-          TopNavBarComponent={<TopNavbar title={tale[0].title} />}
-          imageStyle={styles.headerImage}
-          disableScale={true}
+        <ParallaxScrollView
+          style={{ flex: 1, overflow: 'hidden' }}
+          backgroundColor={Colors.dark900}
+          contentBackgroundColor={Colors.dark900}
+          parallaxHeaderHeight={300}
+          renderForeground={() => (
+            <Image source={{ uri: urlFor(tale[0].imageURL).url() }} style={styles.headerImage} />
+          )}
+          stickyHeaderHeight={80}
+          renderFixedHeader={() => <HeaderNavbar title={tale[0].title} />}
+          renderStickyHeader={() => <TopNavbar title={tale[0].title} />}
         >
+          {/* Content */}
           <View style={styles.content}>
             <TaleContent style={styles.blocks} blocks={tale[0].content} />
           </View>
 
           {/* Like and Bookmark Buttons */}
           <View style={styles.actionButtons}>
-            <TouchableOpacity onPress={handleLike}>
-              {/* <Image
-                source={
-                  isLiked ? require('../../assets/liked.png') : require('../../assets/like.png')
-                }
-                style={styles.likeButton}
-              /> */}
+            <TouchableOpacity
+              disabled={isLiked ? true : false}
+              style={styles.button}
+              onPress={handleLike}
+            >
+              <Icon
+                name={isLiked ? 'heart' : 'heart-outline'}
+                size={23}
+                color={isLiked ? Colors.red : Colors.white}
+              />
+              <Text style={styles.buttonText}>{isLiked ? 'Liked' : 'Like'}</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={handleBookmark}>
-              {/* <Image
-                source={
-                  isBookmarked
-                    ? require('../../assets/bookmarked.png')
-                    : require('../../assets/bookmark.png')
-                }
-                style={styles.bookmarkButton}
-              /> */}
+            <TouchableOpacity style={styles.button} onPress={handleBookmark}>
+              <Icon
+                name={isBookmarked ? 'bookmark' : 'bookmark-outline'}
+                size={23}
+                color={isBookmarked ? Colors.yellow : Colors.white}
+              />
+              <Text style={styles.buttonText}>{isBookmarked ? 'Bookmarked' : 'Bookmark'}</Text>
             </TouchableOpacity>
           </View>
-        </AnimatedScrollView>
+        </ParallaxScrollView>
       )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  infoContainer: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.white,
+  },
+  author: {
+    fontSize: 16,
+    color: Colors.gray,
+    paddingBottom: 5,
+  },
+  category: {
+    fontSize: 16,
+    color: Colors.gray,
+    paddingBottom: 5,
+  },
+  likeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  likeCount: {
+    fontSize: 14,
+    color: Colors.red,
+    marginLeft: 5,
+  },
   container: {
     flex: 1,
-    backgroundColor: Colors.primaryBackground,
+    backgroundColor: Colors.dark900,
   },
   headerImage: {
     height: 300,
@@ -97,19 +141,23 @@ const styles = StyleSheet.create({
   },
   actionButtons: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     alignItems: 'center',
     padding: 20,
   },
-  likeButton: {
-    width: 32,
-    height: 32,
-    resizeMode: 'contain',
+  button: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    backgroundColor: Colors.black,
+    borderRadius: 5,
+    padding: 10,
   },
-  bookmarkButton: {
-    width: 32,
-    height: 32,
-    resizeMode: 'contain',
+  buttonText: {
+    color: Colors.white,
+    marginLeft: 5,
   },
 });
 
