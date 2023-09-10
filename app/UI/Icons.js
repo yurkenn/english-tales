2292fe2184277863ccdd1da9fd15ace080eeef98
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-09-10 13:53:44 +0300

    Update Icons.js

--- app/UI/Icons.js
+++ app/UI/Icons.js
@@ -1,41 +1,8 @@
+import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 
-export const HomeIcon = () => {
-  return <Ionicons name="home" size={23} color="white" />;
+const Icon = ({ name, size, color }) => {
+  return <Ionicons name={name} size={size} color={color} />;
 };
 
-export const SearchIcon = () => {
-  return <Ionicons name="md-search-sharp" size={23} color="white" />;
-};
-
-export const ListenIcon = () => {
-  return <Ionicons name="play-circle" size={23} color="white" />;
-};
-
-export const SavedIcon = () => {
-  return <Ionicons name="ios-bookmark" size={23} color="white" />;
-};
-
-export const ProfileIcon = () => {
-  return <Ionicons name="person" size={23} color="white" />;
-};
-
-export const LogoutIcon = () => {
-  return <Ionicons name="log-out-outline" size={23} color="white" />;
-};
-
-export const BookmarkOutlineIcon = () => {
-  return <Ionicons name="bookmarks-outline" size={23} color="white" />;
-};
-
-export const PlanetOutlineIcon = () => {
-  return <Ionicons name="planet-outline" size={23} color="white" />;
-};
-
-export const TimeIcon = () => {
-  return <Ionicons name="time-outline" size={16} color="white" />;
-};
-
-export const StarIcon = () => {
-  return <Ionicons name="star-outline" size={16} color="white" />;
-};
+export default Icon;
