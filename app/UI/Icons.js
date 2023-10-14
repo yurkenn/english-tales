cf4e7d5dc6bacc5d8fb135b0b873708deeb4d7bb
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-10-15 01:26:47 +0300

    Delete Icons.js

--- app/UI/Icons.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
-
-const Icon = ({ name, size, color }) => {
-  return <Ionicons name={name} size={size} color={color} />;
-};
-
-export default Icon;
