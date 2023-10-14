b10fc3daa641672c6d21ad5f6d96a392fb4ea5bf
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-10-15 01:26:28 +0300

    Update MyStories.js

--- app/components/Home/MyStories.js
+++ app/components/Home/MyStories.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import BookmarkButton from '../../UI/BookmarkButton';
 import { Colors } from '../../constants/colors';
 
 const MyStories = () => {
