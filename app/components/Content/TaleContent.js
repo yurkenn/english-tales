0b58a1d371fec3ac1460d01b4cd3398363dfe617
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-10-15 01:26:23 +0300

    Update TaleContent.js

--- app/components/Content/TaleContent.js
+++ app/components/Content/TaleContent.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     color: Colors.white,
   },
   normal: {
-    fontSize: 16,
+    fontSize: 18,
     color: Colors.white,
     paddingBottom: 10,
     letterSpacing: 0.5,
