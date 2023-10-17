b4233d00448824029becefcca030e2707a88bccd
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-10-17 03:31:23 +0300

    Update Saved.js

--- app/screens/Saved.js
+++ app/screens/Saved.js
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { useBookmark } from '../store/BookmarkContext';
+import SavedCard from '../components/Saved/SavedCard';
 
 const Saved = () => {
+  const { bookmarks } = useBookmark();
+
+  const renderItem = ({ item }) => <SavedCard data={item} />;
+
   return (
     <View>
       <Text>Saved</Text>
+      <FlatList data={bookmarks} renderItem={renderItem} />
     </View>
   );
 };
