ef6d5175eae8ce3983c38810804ba2cea034e751
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-10-20 03:26:05 +0300

    Update Saved.js

--- app/screens/Saved.js
+++ app/screens/Saved.js
@@ -1,22 +1,58 @@
 import React from 'react';
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { Animated, FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useBookmark } from '../store/BookmarkContext';
 import SavedCard from '../components/Saved/SavedCard';
+import { Colors } from '../constants/colors';
 
-const Saved = () => {
-  const { bookmarks } = useBookmark();
+const Saved = ({ navigation }) => {
+  const { bookmarks, removeBookmark } = useBookmark();
   console.log(bookmarks);
 
-  const renderItem = ({ item }) => <SavedCard data={item} />;
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate('Detail', {
+          data: item,
+        })
+      }
+    >
+      <SavedCard data={item} onDelete={removeBookmark} />
+    </TouchableOpacity>
+  );
 
   return (
-    <View>
-      <Text>Saved</Text>
-      <FlatList data={bookmarks} renderItem={renderItem} />
-    </View>
+    <Animated.View style={styles.container}>
+      {bookmarks.length > 0 ? (
+        <FlatList data={bookmarks} renderItem={renderItem} />
+      ) : (
+        <View style={styles.emptyContainer}>
+          <Text style={styles.emptyText}>You haven't saved any tales yet</Text>
+        </View>
+      )}
+    </Animated.View>
   );
 };
 
 export default Saved;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: Colors.white,
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyText: {
+    fontSize: 18,
+    color: '#666',
+  },
+});
