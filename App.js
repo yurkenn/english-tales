e793258d547724b05e3c43ac5efc7026ae334bf3
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-10-20 03:25:37 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,17 +3,20 @@ import Navigation from './app/navigation';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './app/store/AuthContext';
 import BookmarkProvider from './app/store/BookmarkContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-url-polyfill/auto';
 
 const App = () => {
   return (
     <>
       <StatusBar style="light" />
-      <AuthProvider>
-        <BookmarkProvider>
-          <Navigation />
-        </BookmarkProvider>
-      </AuthProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <AuthProvider>
+          <BookmarkProvider>
+            <Navigation />
+          </BookmarkProvider>
+        </AuthProvider>
+      </GestureHandlerRootView>
     </>
   );
 };
