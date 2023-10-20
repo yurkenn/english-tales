50c9f12715839fdc763056374a917b3e6d331826
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-10-20 03:26:12 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// Optionally import the services that you want to use
 import { getAuth } from 'firebase/auth';
-// import {...} from "firebase/database";
 import { getFirestore } from 'firebase/firestore';
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
 import Config from 'react-native-config';
 
-// Initialize Firebase
 const firebaseConfig = {
   apiKey: Config.API_KEY,
   authDomain: Config.AUTH_DOMAIN,
