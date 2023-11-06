8f3c92df251424814e5a9f5702870c4dc6bb1adb
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-11-07 01:58:35 +0300

    Update Profile.js

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,187 +1,95 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AuthContext } from '../store/AuthContext';
 import { Colors } from '../constants/colors';
 import { useFontSize } from '../store/FontSizeContext';
-import { doc, onSnapshot } from 'firebase/firestore';
-import { firestore } from '../../firebaseConfig';
+import LoadingAnimation from '../components/Animations/LoadingAnimation';
+import CustomButton from '../components/CustomButton';
+import FontSizeSettings from '../components/Profile/FontSizeSettings';
+import ProfileDetails from '../components/Profile/ProfileDetails';
 
 const ProfileScreen = () => {
   const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const authContext = useContext(AuthContext);
-
+  const { handleLogout } = useContext(AuthContext);
   const { changeFontSize, fontSize } = useFontSize();
-
-  const handleLogout = () => {
-    authContext.handleLogout();
-  };
 
   useEffect(() => {
     const getUserData = async () => {
+      setLoading(true);
       try {
-        const userJson = await AsyncStorage.getItem('@user');
-        if (userJson) {
-          // If cached data is available, set it to the state
-          const user = JSON.parse(userJson);
-          setUserData(user);
-        }
-
-        // Fetch data from Firestore and update the cache
-        const userRef = doc(firestore, 'users', authContext.userInfo.uid);
-        onSnapshot(userRef, (docSnapshot) => {
-          if (docSnapshot.exists()) {
-            const user = docSnapshot.data();
-            console.log('user', user);
-            setUserData(user);
-            // Update the AsyncStorage cache
-            AsyncStorage.setItem('@user', JSON.stringify(user));
-          }
-        });
+        const user = await retrieveUserData();
+        setUserData(user);
       } catch (error) {
-        console.error('Error retrieving user data:', error);
+        console.error('Error retrieving user data from AsyncStorage:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
     getUserData();
-  }, [authContext.userInfo.uid]);
+  }, []);
 
-  console.log('userData', userData);
+  const retrieveUserData = async () => {
+    const userJson = await AsyncStorage.getItem('@user');
+    return userJson != null ? JSON.parse(userJson) : null;
+  };
+
+  const clearAllData = async () => {
+    try {
+      await AsyncStorage.clear();
+      setUserData(null); // Update the state to reflect the cleared data
+      alert('All data cleared'); // Inform the user
+    } catch (error) {
+      console.error('Error clearing AsyncStorage data:', error);
+    }
+  };
+
+  if (loading) {
+    return <LoadingAnimation />;
+  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.profileContainer}>
-        {userData?.photoURL ? (
-          <Image style={styles.profileImage} source={{ uri: userData.photoURL }} />
-        ) : (
-          <Image
-            source={require('../../assets/images/blank-profile.png')}
-            style={styles.profileImage}
-          />
-        )}
-        <Text style={styles.displayName}>
-          {userData?.displayName ? null : `${userData?.firstName} ${userData?.lastName}`}
-        </Text>
-        <Text style={styles.email}>{userData?.email}</Text>
-        <View style={styles.fontSizeContainer}>
-          <Text style={styles.fontSizeText}>Change Fontsize : {fontSize}</Text>
-          <View style={styles.settingsContainer}>
-            <TouchableOpacity
-              onPress={() => changeFontSize(Number(fontSize + 1))} // Increase font size
-              style={styles.plusButton}
-            >
-              <Text style={styles.plusText}>+</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => changeFontSize(Number(fontSize - 1))} // Decrease font size
-              style={styles.minusButton}
-            >
-              <Text style={styles.minusText}>-</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={handleLogout}>
-            <Text style={styles.buttonText}>Logout</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <ProfileDetails userData={userData} />
+      <FontSizeSettings changeFontSize={changeFontSize} fontSize={fontSize} />
+      <CustomButton
+        title="Clear Data"
+        style={[styles.button, styles.clearDataButton]}
+        onPress={clearAllData}
+        textStyle={styles.fontButtonText}
+      />
+      <CustomButton
+        title="Logout"
+        style={[styles.button, styles.logoutButton]}
+        onPress={handleLogout}
+        textStyle={styles.fontButtonText}
+      />
     </View>
   );
 };
 
-export default ProfileScreen;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50,
+    padding: 20,
+    backgroundColor: Colors.dark900,
   },
-  profileContainer: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  profileImage: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    marginBottom: 20,
-  },
-  displayName: {
-    fontSize: 20,
+  fontButtonText: {
+    color: Colors.white,
     fontWeight: 'bold',
-    marginBottom: 10,
-    color: Colors.white,
+    letterSpacing: 1,
   },
-  email: {
-    fontSize: 16,
-    color: 'gray',
-  },
-  fontSizeContainer: {
-    flexDirection: 'row',
-    backgroundColor: Colors.dark500,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginTop: 20,
-    padding: 10,
-    borderRadius: 5,
-    width: '80%',
-  },
-  fontSizeText: {
-    fontSize: 20,
-    color: Colors.white,
-  },
-  settingsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginTop: 10,
-  },
-  plusButton: {
-    backgroundColor: Colors.dark900,
-    borderRadius: 6,
-    width: 35,
-    height: 35,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 10,
-  },
-  plusText: {
-    fontSize: 24,
-    color: Colors.white,
-  },
-  minusButton: {
-    backgroundColor: Colors.dark900,
-    borderRadius: 6,
-    width: 35,
-    height: 35,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  minusText: {
-    fontSize: 24,
-    color: Colors.white,
-  },
-  buttonContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
+  clearDataButton: {
+    backgroundColor: Colors.red,
+    marginTop: 'auto',
     marginBottom: 20,
-    width: '100%',
   },
-  button: {
+  logoutButton: {
     backgroundColor: Colors.dark500,
-    borderRadius: 6,
-    height: 48,
-    justifyContent: 'center',
-    marginVertical: 10,
-    marginHorizontal: 10,
-  },
-  buttonText: {
-    color: Colors.white,
-    fontSize: 16,
-    fontWeight: '500',
-    lineHeight: 24,
-    textAlign: 'center',
   },
 });
+
+export default ProfileScreen;
