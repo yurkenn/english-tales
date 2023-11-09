0ecdf04029204b5d151eb5e577b708c6337b3aca
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-11-09 22:10:36 +0300

    Add responsive design to FeaturedStories component

--- app/components/Home/FeaturedStories.js
+++ app/components/Home/FeaturedStories.js
@@ -1,10 +1,15 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import { Colors } from '../../constants/colors';
 import { useNavigation } from '@react-navigation/native';
 import { urlFor } from '../../../sanity';
 import FormatReadTime from '../FormatReadTime';
 import Icon from '../Icons';
+import InfoContainer from './InfoContainer';
+
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const IMAGE_WIDTH = WINDOW_WIDTH * 0.5;
+const IMAGE_HEIGHT = IMAGE_WIDTH * 1.25;
 
 const FeaturedStories = ({ data }) => {
   const navigation = useNavigation();
@@ -17,28 +22,11 @@ const FeaturedStories = ({ data }) => {
   const formattedReadTime = FormatReadTime(readTime);
 
   return (
-    <TouchableOpacity onPress={goDetailScreen}>
-      <View>
-        {data && (
-          <View style={styles.container}>
-            <View style={styles.imageContainer}>
-              <Image source={{ uri: urlFor(data.imageURL).url() }} style={styles.image} />
-            </View>
-            <View style={styles.titleContainer}>
-              <Text style={styles.title}>{data.title}</Text>
-            </View>
-            <View style={styles.infoContainer}>
-              <View style={styles.readTimeContainer}>
-                <Icon name={'time-outline'} size={16} color={'white'} />
-                <Text style={styles.readTime}>{formattedReadTime}</Text>
-              </View>
-              <View style={styles.bookmarkContainer}>
-                <Icon name={'heart'} size={16} color={'red'} />
-                <Text style={styles.bookmarks}>{data.tales[0].likes}</Text>
-              </View>
-            </View>
-          </View>
-        )}
+    <TouchableOpacity onPress={goDetailScreen} activeOpacity={0.7} accessibilityRole="button">
+      <View style={styles.container}>
+        <Image source={{ uri: urlFor(data.imageURL).url() }} style={styles.image} />
+        <Text style={styles.title}>{data.title}</Text>
+        <InfoContainer readTime={formattedReadTime} likes={data.tales[0].likes} />
       </View>
     </TouchableOpacity>
   );
@@ -48,7 +36,6 @@ export default FeaturedStories;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: Colors.dark500,
     borderRadius: 10,
     marginHorizontal: 10,
@@ -60,79 +47,15 @@ const styles = StyleSheet.create({
   },
   image: {
     borderRadius: 10,
-    height: 250,
-    width: 190,
+    height: IMAGE_HEIGHT,
+    width: IMAGE_WIDTH,
     resizeMode: 'cover',
-  },
-  authorContainer: {
-    backgroundColor: Colors.dark900,
-    borderRadius: 6,
-    height: 30,
-    bottom: 10,
-    left: 10,
-    padding: 5,
-    position: 'absolute',
-  },
-  author: {
-    color: Colors.white,
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-  titleContainer: {
-    marginVertical: 10,
   },
   title: {
     color: Colors.white,
     fontSize: 18,
     fontWeight: 'bold',
-  },
-  category: {
-    color: Colors.gray,
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-  infoContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  readTimeContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 10,
-  },
-  readTime: {
-    color: Colors.white,
-    fontSize: 12,
-    fontWeight: 'bold',
-    marginLeft: 5,
-  },
-  detailButtonContainer: {
-    flex: 1,
-    alignItems: 'flex-end',
-  },
-  detailButton: {
-    backgroundColor: Colors.dark900,
-    borderRadius: 6,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 60,
-  },
-  detailButtonText: {
-    color: Colors.white,
-    fontSize: 12,
-    fontWeight: 'bold',
-  },
-  bookmarkContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingLeft: 10,
-  },
-  bookmarks: {
-    color: Colors.white,
-    fontSize: 12,
-    fontWeight: 'bold',
-    marginLeft: 5,
+    lineHeight: 24,
+    marginTop: 10,
   },
 });
