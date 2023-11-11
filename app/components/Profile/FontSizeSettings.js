06449fdab66b00b9d05b19dd13a7c499289cf036
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-11-11 03:42:16 +0300

    Refactor FontSizeSettings component with Slider

--- app/components/Profile/FontSizeSettings.js
+++ app/components/Profile/FontSizeSettings.js
@@ -1,64 +1,56 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import CustomButton from '../CustomButton';
-import { Colors } from '../../constants/colors';
+import Slider from '@react-native-community/slider';
+import { Colors } from '../../constants/colors'; // Make sure you import Colors if needed
 
-const FontSizeSettings = ({ changeFontSize, fontSize }) => (
-  <View style={styles.fontSizeSettingsContainer}>
-    <Text style={styles.fontSizeTitle}>Font Size</Text>
-    <View style={styles.fontSizeAdjustmentContainer}>
-      <CustomButton
-        title="A-"
-        style={[styles.button, styles.fontSizeButton]}
-        textStyle={styles.buttonText}
-        onPress={() => changeFontSize(Math.max(12, fontSize - 1))} // Prevent going below a minimum font size
+const FontSizeSettings = ({ changeFontSize, fontSize }) => {
+  return (
+    <View style={styles.fontSizeSettingsContainer}>
+      <Text style={styles.fontSizeTitle}>Font Size</Text>
+      <Slider
+        style={styles.slider}
+        minimumValue={12}
+        maximumValue={30}
+        step={1}
+        value={fontSize}
+        onValueChange={changeFontSize}
+        minimumTrackTintColor={Colors.white} // Replace with your color
+        maximumTrackTintColor={Colors.grey} // Replace with your color
+        thumbTintColor={Colors.primary} // Replace with your color
       />
       <Text style={[styles.fontSizeText, { fontSize }]}>{fontSize}</Text>
-      <CustomButton
-        title="A+"
-        style={[styles.button, styles.fontSizeButton]}
-        textStyle={styles.buttonText}
-        onPress={() => changeFontSize(fontSize + 1)} // Increase font size
-      />
     </View>
-  </View>
-);
+  );
+};
 
 export default FontSizeSettings;
 
 const styles = StyleSheet.create({
   fontSizeSettingsContainer: {
-    flexDirection: 'column',
-    alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 20,
-    padding: 10,
+    alignItems: 'center',
+    paddingBottom: 20,
     borderRadius: 10,
+    marginTop: 20,
   },
   fontSizeTitle: {
-    color: Colors.gray,
-    fontSize: 16,
-    marginBottom: 8,
+    color: Colors.white, // Replace with your color
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
-  fontSizeAdjustmentContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  fontSizeButton: {
-    backgroundColor: Colors.white,
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-    marginHorizontal: 10,
-    elevation: 2,
+  slider: {
+    width: '100%',
+    height: 40,
   },
   fontSizeText: {
-    color: Colors.white,
-    paddingHorizontal: 15,
-    paddingVertical: 8,
+    color: Colors.white, // Replace with your color
+    backgroundColor: Colors.grey, // Replace with your color
+    paddingHorizontal: 12,
+    paddingVertical: 6,
     borderRadius: 5,
-    backgroundColor: Colors.dark500,
-    minWidth: 40,
-    textAlign: 'center',
+    marginTop: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
