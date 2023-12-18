28c3ac4c36899227abaae841722df4220cadc523
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-12-18 04:35:18 +0300

    Add SplashScreen and hideAsync() method

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navigation from './src/navigation';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './src/store/AuthContext';
@@ -7,11 +7,26 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-url-polyfill/auto';
 import { FontSizeProvider } from './src/store/FontSizeContext';
 import Toast from 'react-native-toast-message';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync().catch((err) =>
+  console.warn(`SplashScreen.preventAutoHideAsync() errored:`, err)
+);
 
 const App = () => {
+  useEffect(() => {
+    async function prepare() {
+      // You might want to do some preparations for your app here
+
+      // When preparation is finished, hide the splash screen
+      await SplashScreen.hideAsync();
+    }
+
+    prepare();
+  }, []);
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar style="auto" />
       <GestureHandlerRootView style={{ flex: 1 }}>
         <AuthProvider>
           <FontSizeProvider>
