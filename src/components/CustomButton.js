7b45dad2611b057916e5f2a8ad277b7b55c1efb8
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-01-01 16:43:20 +0300

    Remove unused import in CustomButton.js

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, Image, View } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
 
 const CustomButton = ({ onPress, title, style, textStyle, imageSource, imageStyle }) => {
   return (
