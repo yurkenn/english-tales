99421a0175e8b30f901f98dfa5e029f9eb0e8546
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-02-10 01:37:32 +0300

    Add onboarding screen to AuthNavigation

--- src/navigation/AuthNavigation.js
+++ src/navigation/AuthNavigation.js
@@ -1,20 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../screens/auth/Login';
 import Signup from '../screens/auth/Signup';
+import Onboarding from '../screens/auth/Onboarding';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
 const AuthNavigation = () => {
+  const [showOnboarding, setShowOnboarding] = useState(true);
+  useEffect(() => {
+    const checkIfOnboardingShown = async () => {
+      try {
+        const value = await AsyncStorage.getItem('onboardingShown');
+        if (value !== null) {
+          setShowOnboarding(false);
+        } else {
+          await AsyncStorage.setItem('onboardingShown', 'true');
+        }
+      } catch (error) {
+        console.error('Error retrieving data from AsyncStorage: ', error);
+      }
+    };
+
+    checkIfOnboardingShown();
+  }, []);
+
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Signup" component={Signup} />
+      {showOnboarding ? (
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
