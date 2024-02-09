6b7c5ac2134461401f17516fa6965d09a2dd5faa
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-02-10 01:37:29 +0300

    Add ImageList component

---
+++ src/components/Onboarding/ImageList.js
@@ -0,0 +1,7 @@
+const Images = {
+  first: require('../../../assets/images/onboarding1.jpeg'),
+  second: require('../../../assets/images/onboarding2.jpeg'),
+  third: require('../../../assets/images/onboarding3.jpeg'),
+};
+
+export default Images;
