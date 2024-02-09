72ab1a200b1d1c84697b9bcc38eb091e4fb8b8a0
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-02-10 01:37:26 +0300

    Add style prop to Icon component

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 
-const Icon = ({ name, size, color }) => {
-  return <Ionicons name={name} size={size} color={color} />;
+const Icon = ({ name, size, color, style }) => {
+  return <Ionicons name={name} size={size} color={color} style={style} />;
 };
 
 export default Icon;
