808bba7b451eff3ac25b88405546b10b362e97f2
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-02-10 01:37:41 +0300

    Remove unnecessary padding in Signup screen

--- src/screens/auth/Signup.js
+++ src/screens/auth/Signup.js
@@ -166,7 +166,6 @@ const windowHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: windowWidth * 0.05,
     justifyContent: 'center',
     alignItems: 'center',
   },
