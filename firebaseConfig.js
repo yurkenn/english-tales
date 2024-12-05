b31c4925e2bb06f6e83e0f098c9e3e60cd2ab00e
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-12-05 21:12:11 +0300

    update firebaseConfig and update packages

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
@@ -12,4 +13,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export { auth };
