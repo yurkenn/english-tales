479d825db0a06e49b314dd12662e7bb26fc5b200
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-12-08 21:31:05 +0300

    Update style

--- src/Detail/BookmarkButton.js
+++ src/Detail/BookmarkButton.js
@@ -1,15 +1,15 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { Colors } from '../constants/colors';
 import Icon from '../components/Icons';
 
 const BookmarkButton = ({ isBookmarked, handleBookmark }) => {
   return (
-    <TouchableOpacity onPress={handleBookmark}>
+    <TouchableOpacity onPress={handleBookmark} style={styles.container}>
       <Icon
         name={isBookmarked ? 'bookmark' : 'bookmark-outline'}
         size={24}
-        color={isBookmarked ? Colors.yellow : Colors.white}
+        color={isBookmarked ? Colors.primary : Colors.white}
       />
     </TouchableOpacity>
   );
@@ -17,4 +17,9 @@ const BookmarkButton = ({ isBookmarked, handleBookmark }) => {
 
 export default BookmarkButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 4,
+    padding: 4,
+  },
+});
