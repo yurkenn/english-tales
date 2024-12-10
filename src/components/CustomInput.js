b78353b18062abd98b5eba2d45471e2d0c325b80
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-12-10 03:11:08 +0300

    update dimensions

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,5 +1,8 @@
-import { View, TextInput, Dimensions, StyleSheet } from 'react-native';
+// src/components/CustomInput.js
+import { View, TextInput, StyleSheet } from 'react-native';
 import Icon from './Icons';
+import { wp, hp, fontSizes, spacing, layout } from '../utils/dimensions';
+import { Colors } from '../constants/colors';
 
 const CustomInput = ({ placeholder, onChangeText, value, isSecure, icon, onPress, style }) => {
   return (
@@ -17,26 +20,25 @@ const CustomInput = ({ placeholder, onChangeText, value, isSecure, icon, onPress
   );
 };
 
-export default CustomInput;
-
-const { width, height } = Dimensions.get('window');
-
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    width: width * 0.8,
-    height: height * 0.06,
+    width: wp(80),
+    height: hp(6),
     borderWidth: 1,
     borderColor: '#333333',
-    borderRadius: 10,
+    borderRadius: layout.borderRadius,
   },
   input: {
     flex: 1,
-    paddingLeft: width * 0.02,
-    color: 'white',
+    paddingLeft: spacing.md,
+    color: Colors.white,
+    fontSize: fontSizes.md,
   },
   icon: {
-    marginRight: 10,
+    marginRight: spacing.sm,
   },
 });
+
+export default CustomInput;
