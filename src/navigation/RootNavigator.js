0d19522c52fc0d9038d0c814784f7c4b4a1bd510
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-12-20 01:10:31 +0300

    Refactor RootNavigator to use Redux for authentication state management; add loading screen during auth check

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,29 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { AuthContext } from '../store/AuthContext';
+import { useDispatch, useSelector } from 'react-redux';
 import { AuthStack } from './stacks/AuthStack';
 import { MainStack } from './stacks/MainStack';
-import NavigationErrorBoundary from './NavigationErrorBoundary';
 import { Colors } from '../constants/colors';
+import { checkAuthState } from '../store/slices/authSlice';
+import LoadingScreen from '../components/LoadingScreen'; // You'll need to create this
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
-  const { userInfo } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const { user, loading } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(checkAuthState());
+  }, [dispatch]);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
 
   return (
-    <NavigationErrorBoundary>
-      <NavigationContainer theme={{ colors: { background: Colors.dark900 } }}>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {userInfo ? (
-            <Stack.Screen name="Main" component={MainStack} />
-          ) : (
-            <Stack.Screen name="Auth" component={AuthStack} />
-          )}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NavigationErrorBoundary>
+    <NavigationContainer theme={{ colors: { background: Colors.dark900 } }}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {user ? (
+          <Stack.Screen name="Main" component={MainStack} />
+        ) : (
+          <Stack.Screen name="Auth" component={AuthStack} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
